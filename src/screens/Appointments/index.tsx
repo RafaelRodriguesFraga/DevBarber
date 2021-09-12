6425356a10fe68e441fc410ef6c05aae6cab13c9
Rafael Rodrigues Fraga <ti.rafaelfraga@gmail.com>
2021-09-12 15:43:27 -0300

    Implements Appointments Screen

--- src/screens/Appointments/index.tsx
+++ src/screens/Appointments/index.tsx
@@ -1,11 +1,118 @@
-import React from 'react';
-import {Text, View} from 'react-native';
-import {Container} from './style';
+import React, {useState} from 'react';
+import {RefreshControl, Text, View} from 'react-native';
+import {
+  BackButton,
+  Container,
+  HeaderContainer,
+  HeaderText,
+  ScrollViewContainer,
+  AppointmentItem,
+  BarberAvatar,
+  BarberName,
+  BarberInfo,
+  ServiceInfo,
+  ServiceName,
+  ServicePrice,
+  AppointmentDateText,
+  AppointmentHourText,
+  AppointmentInfo,
+  AppointmentDateContainer,
+  AppointmentHourContainer,
+  LoadingIcon,
+} from './style';
+import BackIcon from '../../assets/back.svg';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {useNavigation} from '@react-navigation/native';
+import {BarberProps} from '../../models/props/barberProps';
+import {User} from '../../models/user';
+import {getBarbers} from '../../services/barber.service';
+import {Alert} from 'react-native';
+import {useEffect} from 'react';
 
 const Appointments = () => {
+  const navigation = useNavigation();
+  const [barberList, setBarberList] = useState<BarberProps[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+
+  useEffect(() => {
+    listBarbers();
+  }, []);
+
+  const handleBackButton = () => {
+    navigation.goBack();
+  };
+
+  const listBarbers = async () => {
+    try {
+      setLoading(true);
+
+      const response = await getBarbers();
+      const {data} = response.data;
+
+      setBarberList(data);
+
+      setLoading(false);
+    } catch (error) {
+      alert('Erro ao listar: ' + error);
+      setLoading(false);
+
+      throw error;
+    }
+  };
+
+  const onRefresh = () => {
+    setRefreshing(false);
+    listBarbers();
+  };
+
   return (
     <Container>
-      <Text>TAB APPOINTMENTS</Text>
+      <HeaderContainer>
+        <BackButton onPress={handleBackButton}>
+          <BackIcon width="44" height="44" fill={Colors.white} />
+        </BackButton>
+
+        <HeaderText>Agendamentos</HeaderText>
+      </HeaderContainer>
+
+      <ScrollViewContainer
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }>
+        {loading && <LoadingIcon color="#fff" size="large" />}
+        {!loading && (
+          <>
+            {barberList.map((barber: BarberProps) => (
+              <AppointmentItem key={barber.id} activeOpacity={0.7}>
+                <BarberInfo>
+                  <BarberAvatar
+                    source={{
+                      uri: barber.avatar,
+                    }}
+                  />
+                  <BarberName>{barber.name}</BarberName>
+                </BarberInfo>
+
+                <ServiceInfo>
+                  <ServiceName>Corte Masculino</ServiceName>
+                  <ServicePrice>R$ 29,90</ServicePrice>
+                </ServiceInfo>
+
+                <AppointmentInfo>
+                  <AppointmentDateContainer>
+                    <AppointmentDateText>12/09/2021</AppointmentDateText>
+                  </AppointmentDateContainer>
+
+                  <AppointmentHourContainer>
+                    <AppointmentHourText>13:00</AppointmentHourText>
+                  </AppointmentHourContainer>
+                </AppointmentInfo>
+              </AppointmentItem>
+            ))}
+          </>
+        )}
+      </ScrollViewContainer>
     </Container>
   );
 };
--- src/screens/Appointments/style.ts
+++ src/screens/Appointments/style.ts
@@ -1,9 +1,88 @@
 import styled from 'styled-components/native';
-import { Colors } from '../../shared/colors';
+import {Colors} from '../../shared/colors';
 
 export const Container = styled.SafeAreaView`
-  background-color: ${Colors.primary};
   flex: 1;
-  justify-content: center;
+  background-color: ${Colors.primary};
+`;
+
+export const HeaderContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: 15px;
+`;
+
+export const HeaderText = styled.Text`
+  font-weight: bold;
+  font-size: 18px;
+  color: ${Colors.white};
+`;
+
+export const BackButton = styled.TouchableOpacity``;
+
+export const ScrollViewContainer = styled.ScrollView`
+  flex: 1;
+  padding: 5px 20px 20px 20px;
+`;
+
+export const LoadingIcon = styled.ActivityIndicator`
+    margin-top: 50px;
+`
+
+export const AppointmentItem = styled.TouchableOpacity`
+  background-color: ${Colors.white};
+  padding: 15px;
+  margin-bottom: 20px;
+  border-radius: 20px;
+`;
+
+export const BarberInfo = styled.View`
+  flex-direction: row;
   align-items: center;
 `;
+
+export const BarberAvatar = styled.Image`
+  width: 70px;
+  height: 70px;
+  border-radius: 20px;
+`;
+export const BarberName = styled.Text`
+  font-weight: bold;
+  font-size: 18px;
+  margin-left: 15px;
+`;
+
+export const ServiceInfo = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 15px;
+`;
+
+export const ServiceName = styled.Text`
+  font-weight: bold;
+  font-size: 17px;
+`;
+
+export const ServicePrice = styled(ServiceName)``;
+
+export const AppointmentInfo = styled(ServiceInfo)``;
+
+export const AppointmentDateContainer = styled.View`
+  background-color: ${Colors.blue1};
+  padding: 14px 20px; /* top-bottom left-right */
+  align-items: center;
+  justify-content: center;
+  border-radius: 10px;
+`;
+
+export const AppointmentDateText = styled.Text`
+  font-weight: bold;
+  color: ${Colors.white};
+  font-size: 15px;
+`;
+
+export const AppointmentHourContainer = styled(AppointmentDateContainer)`
+  padding: 14px 16px;
+`;
+
+export const AppointmentHourText = styled(AppointmentDateText)``;
