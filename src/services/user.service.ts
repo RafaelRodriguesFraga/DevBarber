def5cd938aed653060ed1865a8ac4fcb1c7fdc39
Rafael Rodrigues Fraga <ti.rafaelfraga@gmail.com>
2021-09-12 15:43:13 -0300

    Add getAppointments Route

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,28 +1,39 @@
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { Appointment } from "../models/appointment";
-import { axiosPost } from "../shared/axiosFunctions";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {Appointment} from '../models/appointment';
+import {axiosGet, axiosPost} from '../shared/axiosFunctions';
 
 export const setFavorite = async (id: number) => {
-    const token = await AsyncStorage.getItem('token');
-    const endpoint = '/user/favorite'; 
-  
-    try {
-      const response = await axiosPost(endpoint, {id, token});
-      return response;
-    } catch (error) {
-      throw error;
-    }
-  };
-  
-  export const setAppointment = async (appointment: Appointment) => {
-    const token = await AsyncStorage.getItem('token');
-    const endpoint = '/user/appointment';
-  
-    try {
-      const response = await axiosPost(endpoint, {token, appointment});
-      return response;
-    } catch (error) {
-      throw error;
-    }
-  };
-  
+  const token = await AsyncStorage.getItem('token');
+  const endpoint = '/user/favorite';
+
+  try {
+    const response = await axiosPost(endpoint, {id, token});
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const setAppointment = async (appointment: Appointment) => {
+  const token = await AsyncStorage.getItem('token');
+  const endpoint = '/user/appointment';
+
+  try {
+    const response = await axiosPost(endpoint, {token, appointment});
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const getAppointments = async () => {
+  const token = await AsyncStorage.getItem('token');
+  const endpoint = `user/appointments?token=${token}`;
+
+  try {
+    const response = await axiosGet(endpoint);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
