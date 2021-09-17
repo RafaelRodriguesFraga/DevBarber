10016c6dc26c6289d2f4b5896dff1340de32833e
Rafael Rodrigues Fraga <ti.rafaelfraga@gmail.com>
2021-09-17 16:01:49 -0300

    Implements Favorites Tab

--- src/screens/Favorites/index.tsx
+++ src/screens/Favorites/index.tsx
@@ -1,11 +1,83 @@
 import React from 'react';
-import {Text, View} from 'react-native';
-import {Container} from './style';
+import {RefreshControl, Text, View} from 'react-native';
+import {Colors} from '../../shared/colors';
+import {
+  Container,
+  BackButton,
+  HeaderContainer,
+  HeaderText,
+  ScrollViewContainer,
+  LoadingIcon,
+} from './style';
+import BackIcon from '../../assets/back.svg';
+import {useNavigation} from '@react-navigation/native';
+import {useState} from 'react';
+import {useEffect} from 'react';
+import {BarberProps} from '../../models/props/barberProps';
+import {getBarbers} from '../../services/barber.service';
+import BarberItem from '../../components/BarberItem';
 
 const Favorites = () => {
+  const navigation = useNavigation();
+  const [refreshing, setRefreshing] = useState(false);
+
+  const [barberList, setBarberList] = useState<BarberProps[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    listBarbers();
+  }, []);
+
+  const handleBackButton = () => {
+    navigation.goBack();
+  };
+
+  const listBarbers = async () => {
+    try {
+      setLoading(true);
+
+      const response = await getBarbers();
+      const {data} = response.data;
+
+      setBarberList(data);
+
+      setLoading(false);
+    } catch (error) {
+      alert('Erro ao listar: ' + error);
+      setLoading(false);
+
+      throw error;
+    }
+  };
+
+  const onRefresh = () => {
+    setRefreshing(false);
+    listBarbers();
+  };
+
   return (
     <Container>
-      <Text>TAB FAVORITES</Text>
+      <HeaderContainer>
+        <BackButton onPress={handleBackButton}>
+          <BackIcon width="44" height="44" fill={Colors.white} />
+        </BackButton>
+
+        <HeaderText>Favoritos</HeaderText>
+      </HeaderContainer>
+
+      <ScrollViewContainer
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }>
+        {loading && <LoadingIcon color="#fff" size="large" />}
+        {!loading && (
+          <>
+            {barberList.map((barber: BarberProps) => (
+              <BarberItem key={barber.id} user={barber} />
+            ))}
+          </>
+        )}
+      </ScrollViewContainer>
     </Container>
   );
 };
--- src/screens/Favorites/style.ts
+++ src/screens/Favorites/style.ts
@@ -2,8 +2,30 @@ import styled from 'styled-components/native';
 import { Colors } from '../../shared/colors';
 
 export const Container = styled.SafeAreaView`
-  background-color: ${Colors.primary};
   flex: 1;
-  justify-content: center;
-  align-items: center;
+  background-color: ${Colors.primary};
 `;
+
+export const HeaderContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  padding: 15px;
+`;
+
+export const HeaderText = styled.Text`
+  font-weight: bold;
+  font-size: 18px;
+  color: ${Colors.white};
+`;
+
+export const BackButton = styled.TouchableOpacity``;
+
+export const ScrollViewContainer = styled.ScrollView`
+  flex: 1;
+  padding: 5px 20px 20px 20px;
+`;
+
+export const LoadingIcon = styled.ActivityIndicator`
+    margin-top: 50px;
+`
+
